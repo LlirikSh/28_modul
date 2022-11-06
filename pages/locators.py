0d317c7fbef77d739b6f0d25b23ea601585c44f0
Kirill Sheregov <112244472+LlirikSh@users.noreply.github.com>
2022-11-06 11:26:12 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -3,33 +3,33 @@ class AuthLocators:
     AUTH_LOGO = (By.CLASS_NAME, "b-header-b-logo-e-logo")
 
     # Локатор кнопки "Телефон" на стартовой на странице
-    AUTH_START_
+    AUTH_START_PHONE = 
     # Локатор кнопки "Почта" на стартовой на странице
-    AUTH_START_
+    AUTH_START_MAIL = 
     # Локатор кнопки "Логин" на стартовой на странице
-    AUTH_START_
+    AUTH_START_LOGIN = 
     # Локатор кнопки "Лицевой счет" на стартовой на странице
-    AUTH_START_
+    AUTH_START_PERSONAL_ACCOUNT = 
     # Локатор кнопки "Забыл пароль" на стартовой на странице
-    AUTH_START_
+    AUTH_START_FORGOT_PASSWORD = 
     # Локатор кнопки "Войти" на стартовой на странице
-    AUTH_START_
+    AUTH_START_LOGIN = 
     # Локатор кнопки "Пользовательское соглашение" на стартовой на странице
-    AUTH_START_
+    AUTH_START_USER_AGREEMENT = 
     # Локатор кнопки социальных сетей "ВК" на стартовой на странице
-    AUTH_START_
+    AUTH_START_VK = 
      # Локатор кнопки социальных сетей "Однокласники" на стартовой на странице
-    AUTH_START_
+    AUTH_START_OK = 
      # Локатор кнопки социальных сетей "Мой мир" на стартовой на странице
-    AUTH_START_
+    AUTH_START_MY_WORLD = 
      # Локатор кнопки социальных сетей "Google" на стартовой на странице
-    AUTH_START_
+    AUTH_START_GOOGLE = 
      # Локатор кнопки социальных сетей "Яндекс" на стартовой на странице
-    AUTH_START_
+    AUTH_START_YANDEX = 
     # Локатор кнопки "Регистрация" на стартовой на странице
-    AUTH_START_
+    AUTH_START_REGISTER = 
     # Локатор кнопки "Чат" на стартовой на странице
-    AUTH_START_
+    AUTH_START_CHAT = 
   
   (By.XPATH, '//div[@class="pagination-next"]/a[text()="Следующая"]')
   
