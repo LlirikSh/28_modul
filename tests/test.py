9b3754b16e2796fb4bd0aa79c7433ae9288b32f6
Kirill Sheregov <112244472+LlirikSh@users.noreply.github.com>
2022-11-06 13:17:56 +0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ from pages.auth_page import AuthPage
 from tests.test_base import BaseTest
 
 
-   def test_go_to_page_zabil_password(self):
+   def test_go_to_zabil_password(self):
       """Тест проверяет, что на главной странице сайта кнопка "Забыл пароль" переводит на страницу 'востановления пароля' """
       self.authPage = AuthPage(self.driver)
       START_FORGOT_PASSWORD = self.authPage.element_are_present(AuthLocators.AUTH_START_FORGOT_PASSWORD)
@@ -14,7 +14,7 @@ from tests.test_base import BaseTest
       ZABIL_PASSWORD_URL = self.authPage.get_url()
       assert ZABIL_PASSWORD_URL == TestData.ZABIL_PASSWORD_URL
  
-   def test_go_to_page_putorder(self):
+   def test_go_to_user_agreement(self):
       """Тест проверяет, что на главной странице сайта кнопка "пользовательского соглашения" переводит на страницу 'пользовательского соглашения' """
       self.authPage = AuthPage(self.driver)
       START_USER_AGREEMENT = self.authPage.element_are_present(AuthLocators.AUTH_START_USER_AGREEMENT)
@@ -24,7 +24,7 @@ from tests.test_base import BaseTest
       USER_AGREEMENT_URL = self.authPage.get_url()
       assert USER_AGREEMENT_URL == TestData.USER_AGREEMENT_URL
       
-    def test_go_to_page_putorder(self):
+    def test_go_to_forgot_password(self):
       """Тест проверяет, что на главной странице сайта кнопка "Зарегистрироваться" переводит на страницу 'регистрации нового пользователя' """
       self.authPage = AuthPage(self.driver)
       START_FORGOT_PASSWORD = self.authPage.element_are_present(AuthLocators.AUTH_START_FORGOT_PASSWORD)
@@ -34,7 +34,7 @@ from tests.test_base import BaseTest
       REGISTER_URL = self.authPage.get_url()
       assert REGISTER_URL == TestData.REGISTER_URL
       
-   def test_go_to_page_putorder(self):
+   def test_go_to_vk(self):
       """Тест проверяет, что на главной странице сайта кнопка социальных сетей "ВК" переводит на страницу 'регистрация с помощью социальной сети VK' """
       self.authPage = AuthPage(self.driver)
       START_VK = self.authPage.element_are_present(AuthLocators.AUTH_START_VK)
@@ -43,6 +43,26 @@ from tests.test_base import BaseTest
       # открывается страница "Отложенные", получаем URL новой страницы и сравниваем с данными из config.py
       VK_URL = self.authPage.get_url()
       assert VK_URL == TestData.VK_URL
+      
+   def test_go_to_ok(self):
+      """Тест проверяет, что на главной странице сайта кнопка социальных сетей "Однокласники" переводит на страницу 'регистрация с помощью социальной сети OK' """
+      self.authPage = AuthPage(self.driver)
+      START_OK = self.authPage.element_are_present(AuthLocators.AUTH_START_OK)
+      assert START_OK.is_displayed(), "Элемент не виден на дисплее при ширине окна более 1020"
+      self.authPage.hover_cursor_click(AuthLocators.AUTH_START_OK)
+      # открывается страница "Отложенные", получаем URL новой страницы и сравниваем с данными из config.py
+      OK_URL = self.authPage.get_url()
+      assert OK_URL == TestData.OK_URL
+      
+   def test_go_to_my_world(self):
+      """Тест проверяет, что на главной странице сайта кнопка социальных сетей "Мой мир" переводит на страницу 'регистрация с помощью социальной сети mail.ru' """
+      self.authPage = AuthPage(self.driver)
+      START_MY_WORLD = self.authPage.element_are_present(AuthLocators.AUTH_START_MY_WORLD)
+      assert START_MY_WORLD.is_displayed(), "Элемент не виден на дисплее при ширине окна более 1020"
+      self.authPage.hover_cursor_click(AuthLocators.AUTH_START_MY_WORLD)
+      # открывается страница "Отложенные", получаем URL новой страницы и сравниваем с данными из config.py
+      MAIL_URL = self.authPage.get_url()
+      assert MAIL_URL == TestData.MAIL_URL
 
   
 
