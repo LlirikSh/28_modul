214d26bc2f9c3be740b95530354f55f7f882b7fb
Kirill Sheregov <112244472+LlirikSh@users.noreply.github.com>
2022-11-06 11:56:16 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -10,13 +10,7 @@ class AuthLocators:
     AUTH_START_LOGIN = (By.XPATH, '//span[text()="Логин"]')
     # Локатор кнопки "Лицевой счет" на стартовой на странице
     AUTH_START_PERSONAL_ACCOUNT = (By.XPATH, '//span[text()="Лицевой счёт"]')
-    # Локатор кнопки "Забыл пароль" на стартовой на странице
-    AUTH_START_FORGOT_PASSWORD = 
-    # Локатор кнопки "Войти" на стартовой на странице
-    AUTH_START_LOGIN = 
-    # Локатор кнопки "Пользовательское соглашение" на стартовой на странице
-    AUTH_START_USER_AGREEMENT = 
-    # Локатор кнопки социальных сетей "ВК" на стартовой на странице
+       # Локатор кнопки социальных сетей "ВК" на стартовой на странице
     AUTH_START_VK = (By.XPATH, '//div[@class="social-providers"]/a[@alt="ВКонтакте"]')
      # Локатор кнопки социальных сетей "Однокласники" на стартовой на странице
     AUTH_START_OK = (By.XPATH, '//div[@class="social-providers"]/a[@alt="Одноклассники.ru"]')
@@ -27,9 +21,15 @@ class AuthLocators:
      # Локатор кнопки социальных сетей "Яндекс" на стартовой на странице
     AUTH_START_YANDEX = (By.XPATH, '//div[@class="social-providers"]/a[@alt="Yandex.ru"]')
     # Локатор кнопки "Регистрация" на стартовой на странице
-    AUTH_START_REGISTER = 
+    AUTH_START_REGISTER = (By.XPATH, '//div[@class="login-form__register-con"]/a[text()=" Зарегистрироваться "]')
     # Локатор кнопки "Чат" на стартовой на странице
     AUTH_START_CHAT = 
+     # Локатор кнопки "Забыл пароль" на стартовой на странице
+    AUTH_START_FORGOT_PASSWORD =  (By.CLASS_NAME, "b-header-b-personal-e-list-item")
+    # Локатор кнопки "Войти" на стартовой на странице
+    AUTH_START_LOGIN = (By.CLASS_NAME, "rt-link rt-link--orange rt-link--muted login-form__forgot-pwd login-form__forgot-pwd--muted")
+    # Локатор кнопки "Пользовательское соглашение" на стартовой на странице
+    AUTH_START_USER_AGREEMENT = (By.XPATH, '//div[@class="auth-policy"]/a[text()="пользовательского соглашения"]')
   
   (By.XPATH, '//div[@class="pagination-next"]/a[text()="Следующая"]')
   
