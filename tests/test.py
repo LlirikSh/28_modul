94f4cca09db33509c4656b5ab009b3ab20af54d3
Kirill Sheregov <112244472+LlirikSh@users.noreply.github.com>
2022-11-06 14:35:40 +0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -113,14 +113,14 @@ from tests.test_base import BaseTest
       # открывается страница "Отложенные", получаем URL новой страницы и сравниваем с данными из config.py
       LOGIN_URL = self.authPage.get_url()
       assert LOGIN_URL == TestData.LOGIN_URL        
-      
-def test_go_to_page_cart(self):
-      """Тест проверяет, что в шапке сайта кнопка "Корзина" переводит на страницу START_URL + '.../'"""
-      self.authPage = AuthPage(self.driver)
-      HEADER_BUTTON_CART = self.authPage.element_are_present(AuthLocators.AUTH_HEADER_BUTTON_CART)
-      assert HEADER_BUTTON_CART.is_displayed(), "Элемент не виден на дисплее при ширине окна более 1020"
-      self.authPage.hover_cursor_click(AuthLocators.AUTH_HEADER_BUTTON_CART)
-      # открывается страница "Корзина", получаем URL новой страницы и сравниваем с данными из config.py
-      CART_URL = self.authPage.get_url()
-      assert CART_URL == TestData.CART_URL
 
+    def test_go_to_registration_by_personal_account(self):
+      """Тест проверяет, что на главной странице сайта кнопка способ регистрации "Лицевой счет" переводит на страницу 'регистрация с помощью лицевого счета и пароля' """
+      self.authPage = AuthPage(self.driver)
+      START_PERSONAL_ACCOUNT = self.authPage.element_are_present(AuthLocators.AUTH_START_PERSONAL_ACCOUNT)
+      assert START_PERSONAL_ACCOUNT.is_displayed(), "Элемент не виден на дисплее при ширине окна более 1020"
+      self.authPage.hover_cursor_click(AuthLocators.AUTH_START_PERSONAL_ACCOUNT)
+      # открывается страница "Отложенные", получаем URL новой страницы и сравниваем с данными из config.py
+      LOGIN_URL = self.authPage.get_url()
+      assert LOGIN_URL == TestData.LOGIN_URL        
+      
