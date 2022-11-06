cd552d737148718284b95cc72fad2954215f7124
Kirill Sheregov <112244472+LlirikSh@users.noreply.github.com>
2022-11-06 13:01:49 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -12,22 +12,21 @@ class AuthLocators:
     AUTH_START_PERSONAL_ACCOUNT = (By.XPATH, '//span[text()="Лицевой счёт"]')
     # Локатор кнопки социальных сетей "ВК" на стартовой на странице
     AUTH_START_VK = (By.XPATH, '//div[@class="social-providers"]/a[@alt="ВКонтакте"]')
-     # Локатор кнопки социальных сетей "Однокласники" на стартовой на странице
+    # Локатор кнопки социальных сетей "Однокласники" на стартовой на странице
     AUTH_START_OK = (By.XPATH, '//div[@class="social-providers"]/a[@alt="Одноклассники.ru"]')
-     # Локатор кнопки социальных сетей "Мой мир" на стартовой на странице
+    # Локатор кнопки социальных сетей "Мой мир" на стартовой на странице
     AUTH_START_MY_WORLD = (By.XPATH, '//div[@class="social-providers"]/a[@alt="Mail.ru"]')
-     # Локатор кнопки социальных сетей "Google" на стартовой на странице
+    # Локатор кнопки социальных сетей "Google" на стартовой на странице
     AUTH_START_GOOGLE = (By.XPATH, '//div[@class="social-providers"]/a[@alt="Google+"]')
-     # Локатор кнопки социальных сетей "Яндекс" на стартовой на странице
+    # Локатор кнопки социальных сетей "Яндекс" на стартовой на странице
     AUTH_START_YANDEX = (By.XPATH, '//div[@class="social-providers"]/a[@alt="Yandex.ru"]')
     # Локатор кнопки "Регистрация" на стартовой на странице
     AUTH_START_REGISTER = (By.XPATH, '//div[@class="login-form__register-con"]/a[text()=" Зарегистрироваться "]')
     # Локатор кнопки "Чат" на стартовой на странице
     AUTH_START_CHAT = (By.CLASS_NAME, "omnichat-bar-root svelte-16eyo30")
-     # Локатор кнопки "Забыл пароль" на стартовой на странице
+    # Локатор кнопки "Забыл пароль" на стартовой на странице
     AUTH_START_FORGOT_PASSWORD =  (By.CLASS_NAME, "b-header-b-personal-e-list-item")
     # Локатор кнопки "Войти" на стартовой на странице
     AUTH_START_LOGIN = (By.CLASS_NAME, "rt-link rt-link--orange rt-link--muted login-form__forgot-pwd login-form__forgot-pwd--muted")
     # Локатор кнопки "Пользовательское соглашение" на стартовой на странице
-    AUTH_START_USER_AGREEMENT = (By.XPATH, '//div[@class="auth-policy"]/a[text()="пользовательского соглашения"]')
-    
+    AUTH_START_USER_AGREEMENT = (By.XPATH, '//div[@class="auth-policy"]/a[text()="пользовательского соглашения"]')  
